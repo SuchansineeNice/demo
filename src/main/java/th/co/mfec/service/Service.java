b308829bf019685c095aadee47f3075d1b196bf1
Suchansinee <nunnicee@gmail.com>
2019-03-27 15:40:55 +0700

    commit demo project

---
+++ src/main/java/th/co/mfec/service/Service.java
@@ -0,0 +1,11 @@
+package th.co.mfec.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service {
+	
+	public void helloWorld() {
+		System.out.print("Hello World!");
+	}
+}
